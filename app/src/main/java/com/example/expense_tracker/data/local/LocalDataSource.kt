dc3c86e5af4169e6877d6079dc361cfd5dba1f94
Ozan <ozancangu24@gmail.com>
2023-04-05 18:35:33 +0300

    -LocalDataSource.kt created

---
+++ app/src/main/java/com/example/expense_tracker/data/local/LocalDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.expense_tracker.data.local
+
+import androidx.lifecycle.LiveData
+import com.example.expense_tracker.data.entity.Expense
+import com.example.expense_tracker.data.entity.ExpenseDao
+import javax.inject.Inject
+
+class LocalDataSource@Inject constructor(private val expenseDao: ExpenseDao) {
+
+    fun getAllExpense():LiveData<List<Expense>>{
+        return expenseDao.getAllExpenseList()
+    }
+
+    suspend fun insertExpense(expense: Expense){
+        return expenseDao.insertExpense(expense)
+    }
+
+    suspend fun deleteExpense(expense: Expense){
+        return expenseDao.deleteExpense(expense)
+    }
+}
