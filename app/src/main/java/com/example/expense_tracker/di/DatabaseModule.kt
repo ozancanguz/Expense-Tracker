977c87f29a2452bb409a47ae691ab91515129c98
Ozan <ozancangu24@gmail.com>
2023-04-05 18:32:39 +0300

    -Di DatabaseModule.kt created

---
+++ app/src/main/java/com/example/expense_tracker/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.example.expense_tracker.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.expense_tracker.data.entity.ExpenseDao
+import com.example.expense_tracker.data.entity.ExpenseDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ExpenseDatabase::class.java,
+        "product_database"
+    ).build()
+
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ExpenseDatabase) = database.expenseDao()
+}
